05e4e5928a3dab15278e814a3cd05d9c71b45cc0
Benar Isais <benaremail@gmail.com>
2020-07-22 01:31:41 +0800

    better while condition statement

--- Sum of given number (Array)/Sum of given number (Array)/main.cpp
+++ Sum of given number (Array)/Sum of given number (Array)/main.cpp
@@ -3,14 +3,20 @@
 
 using namespace std;
 
+//int faveNumber() {
+//	return 123;
+//}
+
 int main() {
+	//int n = faveNumber();
+
 	int n;
 
 	do
 	{
 		cout << "Enter the correct number of elements from 1 to 100: ";
 		cin >> n;
-	} while (((n < 1) && !(n > 100)) || (!(n < 0) && (n > 100)));
+	}while (!(n > 0 && n <= 100));
 	
 	int x[100];
 	for (int i = 0; i < n; i= i + 1) {
